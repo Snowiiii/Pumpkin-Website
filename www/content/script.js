bf4467a9e529100b455fb5c7fa8fc797c9c68395
Dino <119540345+disnos9@users.noreply.github.com>
2024-09-02 13:30:46 -0400

    v0.4.0 - Latest release Pumpkin Website

---
+++ www/content/script.js
@@ -0,0 +1,26 @@
+// Open the modal and display the clicked image
+function openModal(image) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+
+    modal.style.display = "block";
+     modalImage.src = image.src;
+     modalImage.style.transform = "scale(1)";
+
+     document.addEventListener("keydown", closeModalOnEscape);
+ }
+
+ // Close the modal
+ function closeModal() {
+     const modal = document.getElementById("imageModal");
+     modal.style.display = "none";
+
+     document.removeEventListener("keydown", closeModalOnEscape);
+ }
+
+ // Function to close modal when Escape key is pressed
+ function closeModalOnEscape(event) {
+     if (event.key === "Escape") {
+         closeModal();
+     }
+ }
